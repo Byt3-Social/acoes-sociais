2b8ac768daf32e263de96fd2d633d6ffb5ea1720
Leandro Pereira <leojgpereira@gmail.com>
2023-09-26 16:20:18 -0400

    Commit inicial

---
+++ src/main/java/com/byt3social/acoessociais/AcoesSociaisApplication.java
@@ -0,0 +1,13 @@
+package com.byt3social.acoessociais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcoesSociaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcoesSociaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byt3social/acoessociais/AcoesSociaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byt3social.acoessociais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcoesSociaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
